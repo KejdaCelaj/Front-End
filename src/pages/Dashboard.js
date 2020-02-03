ece331af2714b763168070cf020638ff221d128a
Freelance Development <49110640+KejdaCelaj@users.noreply.github.com>
2020-02-03 14:47:32 -0500

    fixed import error

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from 'react-router-dom';
-import Login from '../components/Login';
-import Register from '../components/Register';
+import Login from '../pages/Login';
+import Register from '../pages/Register';
 
 export default function Welcome() {
   return (
